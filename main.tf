a36f6e09464d650fcfcf61f73df4797a9dcaf616
isakovasvitlana <isakovasvitlana@gmail.com>
2019-10-03 00:59:21 +0300

    Import tcbuild

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "google" {
   credentials = "${var.var_credentials}"
 }
 
-resource  "google_compute_instance" "tc-ci"{
-    name = "tc-ci-server"
+resource  "google_compute_instance" "tcbuild"{
+    name = "TeamCity-tf"
 #provisioner "file" {
 #    source      = "D:\\keys\\united-aura-252016-a5e385393212.json"
 #    destination = "/home/isakovasvitlana/united-aura-252016-a5e385393212.json"
--- variables.tf
+++ variables.tf
@@ -3,10 +3,7 @@ variable "var_project" {
   }
 
 variable "var_credentials" {
-  description = "CREDENTIALS_JSON_PATH"
-
- # default = "/home/DevOpS/united-aura-252016-a5e385393212.json"
- default = "D:\\keys\\united-aura-252016-a5e385393212.json"
-
-
+ # description = "CREDENTIALS_JSON_PATH"
+ default = "/home/DeVOpS/united-aura-252016-a5e385393212.json"
+ #default = "D:\\keys\\united-aura-252016-a5e385393212.json"
 }
